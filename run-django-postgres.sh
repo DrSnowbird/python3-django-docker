9947f744fb8971c56654fd575783cc6d70c0fc03
DrSnowbird <openkbs.org@gmail.com>
2018-07-05 18:07:28 -0400

    update run-django-postres.sh

--- run-django-postgres.sh
+++ run-django-postgres.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 
 sudo docker-compse build
-sudo docker-compose run web django-admin.py startproject composeexample .
+sudo docker-compose run web-django django-admin.py startproject composeexample .
 
+#sudo docker-compose run web-django python manage.py startapp polls
